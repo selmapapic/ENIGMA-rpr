cd635d74cdcb6ead3beb8549d9ceeb63376417c5
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-02-01 19:04:53 +0100

    Otprilike kreiran izgled pocetnog ekrana u fxml-u

--- src/sample/Doctorate.java
+++ src/sample/Doctorate.java
@@ -1,10 +1,9 @@
-import sample.Author;
-import sample.ScientificPaper;
+package sample;
 
 import java.util.Date;
 
 public class Doctorate extends ScientificPaper {
-    
+
     public Doctorate(Author author, Date releaseDate, String category, String title) {
         super(author, releaseDate, category, title);
     }
