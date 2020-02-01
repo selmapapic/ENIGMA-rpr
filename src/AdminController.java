f6fa31c4fa39871be3a30ae03577eb4192f08bb6
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-02-01 21:08:35 +0100

    Napravljen prozor za prijavu administratora

---
+++ src/AdminController.java
@@ -0,0 +1,14 @@
+public class AdminController {
+
+    public void initialize () {
+
+    }
+
+    public void cancelAction () {
+        System.exit(0);
+    }
+
+    public void okAction () {
+
+    }
+}
--- src/MainWinController.java
+++ src/MainWinController.java
@@ -1,8 +1,31 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class MainWinController {
     public Button btnAdmin, btnUser;
 
+    public void adminAction () throws IOException {
+        Stage stageAdmin = new Stage();
+        AdminController controller = new AdminController();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adminWin.fxml"));
+        loader.setController(controller);
+        Parent root = loader.load();
+        stageAdmin.setTitle("Pretraga slike");
+        stageAdmin.setResizable(true);
+        stageAdmin.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stageAdmin.setResizable(false);
+        stageAdmin.show();
+    }
 
+    public void userAction () {
+
+    }
 
 }
