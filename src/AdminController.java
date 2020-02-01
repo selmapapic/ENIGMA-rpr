4bd70075d13eecca1a7362b24b7a5dc05420f649
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-02-01 21:20:24 +0100

    Dodan stylesheet, te validacija za password administratora

--- src/AdminController.java
+++ src/AdminController.java
@@ -1,11 +1,25 @@
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class AdminController {
+    public TextField fldPass;
 
     public void initialize () {
-
+        fldPass.textProperty().addListener((observable, oldPass, newPass) -> {
+            if(!newPass.isEmpty() && newPass.equals("administrator")) {
+                fldPass.getStyleClass().removeAll("invalidField");
+                fldPass.getStyleClass().add("validField");
+            }
+            else {
+                fldPass.getStyleClass().removeAll("validField");
+                fldPass.getStyleClass().add("invalidField");
+            }
+        });
     }
 
     public void cancelAction () {
-        System.exit(0);
+        Stage stage = (Stage) fldPass.getScene().getWindow();
+        stage.close();
     }
 
     public void okAction () {
