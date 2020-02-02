98de308a62e78c674e7c3ce9c8efdf3a11152c53
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-02-02 02:35:39 +0100

    Kreiran izgled upload prozora i controller klasa

--- src/AdminController.java
+++ src/AdminController.java
@@ -1,5 +1,12 @@
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class AdminController {
     public TextField fldPass;
@@ -22,7 +29,19 @@ public class AdminController {
         stage.close();
     }
 
-    public void okAction () {
-
+    public void okAction () throws IOException {
+        System.out.println(fldPass.getStyleClass().toString());
+        if(!fldPass.getStyleClass().contains("invalidField")) {
+            Stage stageUpload = new Stage();
+            UploadController controller = new UploadController();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/uploadWin.fxml"));
+            loader.setController(controller);
+            Parent root = loader.load();
+            stageUpload.setTitle("Upload");
+            stageUpload.setResizable(true);
+            stageUpload.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stageUpload.setResizable(false);
+            stageUpload.show();
+        }
     }
 }
--- src/UploadController.java
+++ src/UploadController.java
@@ -1,2 +1,10 @@
 public class UploadController {
+
+    public void cancelAction () {
+
+    }
+
+    public void submitAction () {
+
+    }
 }
