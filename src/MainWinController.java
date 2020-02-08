33f8d6a71ea4cda24484ac9362523c676cec818e
scelosmano1 <scelosmano1@etf.unsa.ba>
2020-02-08 01:36:16 +0100

    Spojeni kontroleri za sign in i sign up; dodane neke css stvarcice oko validacije

--- src/MainWinController.java
+++ src/MainWinController.java
@@ -1,10 +1,10 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,15 +13,34 @@ import java.io.IOException;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class MainWinController {
-    public Button btnAdmin, btnSignIn, btnRegister, btnSignIn1, btnRegister1;
+    public Button btnSignIn;
     public TextField fldEmail;
     public PasswordField fldPass;
     public UsersDAO dao = UsersDAO.getInstance();
     public AnchorPane anchorSignUp, anchorSignIn;
+    //register
+    public TextField fldName, fldSurname, fldEmailSignUp;
+    public PasswordField fldPassSignUp;
+    public Button btnRegister;
+    public ChoiceBox<String> choiceEduDeg;
+    public ObservableList<String> academicDegree = FXCollections.observableArrayList();
 
+    private boolean isTheStyleClassInvalid (String s) {
+        return s.contains("invalidField");
+    }
+
+    public MainWinController() {
+        academicDegree.add("Srednja skola");
+        academicDegree.add("Bachelor");
+        academicDegree.add("Master");
+        academicDegree.add("Doktorat");
+    }
+
+    @FXML
     public void initialize () {
         anchorSignUp.toBack();
         anchorSignIn.toFront();
+        choiceEduDeg.setItems(academicDegree);
     }
 
     public void adminAction () throws IOException {
@@ -39,44 +58,100 @@ public class MainWinController {
 
     public void openRegisterAction () {
         anchorSignUp.toFront();
+        fldEmail.setText("");
+        fldPass.setText("");
     }
 
     public void openSignInAction () {
         anchorSignIn.toFront();
+        fldName.setText("");
+        fldSurname.setText("");
+        fldEmailSignUp.setText("");
+        fldPassSignUp.setText("");
+        choiceEduDeg.setValue("");
     }
 
-    public void registerAction () throws IOException {
-//        Stage stageUser = new Stage();
-//        RegisterController controller = new RegisterController();
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/userRegister.fxml"));
-//        loader.setController(controller);
-//        Parent root = loader.load();
-//        stageUser.setTitle("Login page");
-//        stageUser.setResizable(true);
-//        stageUser.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-//        stageUser.setResizable(false);
-//        stageUser.show();
+    public void registerAction () {
+        if(fldName.getText().isEmpty() || fldSurname.getText().isEmpty() || fldEmail.getText().isEmpty() || fldPass.getText().isEmpty() || choiceEduDeg.getSelectionModel().getSelectedItem() == null) {
+            if(fldName.getText().isEmpty()) {
+                fldName.getStyleClass().add("myborderedregion");
+                fldName.getStyleClass().removeAll("validField");
+                fldName.getStyleClass().add("invalidField");
+            }
+            else {
+                fldName.getStyleClass().remove("myborderedregion");
+                fldName.getStyleClass().removeAll("invalidField");
+                fldName.getStyleClass().add("validField");
+            }
+
+            if(fldSurname.getText().isEmpty()) {
+                fldSurname.getStyleClass().add("myborderedregion");
+                fldSurname.getStyleClass().removeAll("validField");
+                fldSurname.getStyleClass().add("invalidField");
+            }
+            else {
+                fldSurname.getStyleClass().remove("myborderedregion");
+                fldSurname.getStyleClass().removeAll("invalidField");
+                fldSurname.getStyleClass().add("validField");
+            }
+
+            if(fldEmailSignUp.getText().isEmpty()) {
+                fldEmailSignUp.getStyleClass().add("myborderedregion");
+                fldEmailSignUp.getStyleClass().removeAll("validField");
+                fldEmailSignUp.getStyleClass().add("invalidField");
+            }
+            else {
+                fldEmailSignUp.getStyleClass().remove("myborderedregion");
+                fldEmailSignUp.getStyleClass().removeAll("invalidField");
+                fldEmailSignUp.getStyleClass().add("validField");
+            }
+
+            if(fldPassSignUp.getText().isEmpty()) {
+                fldPassSignUp.getStyleClass().add("myborderedregion");
+                fldPassSignUp.getStyleClass().removeAll("validField");
+                fldPassSignUp.getStyleClass().add("invalidField");
+            }
+            else {
+                fldPassSignUp.getStyleClass().remove("myborderedregion");
+                fldPassSignUp.getStyleClass().removeAll("invalidField");
+                fldPassSignUp.getStyleClass().add("validField");
+            }
+        }
+
+        if(!isTheStyleClassInvalid(fldName.getStyleClass().toString()) && !isTheStyleClassInvalid(fldSurname.getStyleClass().toString()) //ukoliko su sva polja validna, tj nijedno polje nije nevalidno, dodaje se novi korisnik
+                && !isTheStyleClassInvalid(fldEmailSignUp.getStyleClass().toString()) && !isTheStyleClassInvalid(fldPassSignUp.getStyleClass().toString()) && !(choiceEduDeg.getSelectionModel().getSelectedItem() == null)) {
+            User user = new User(fldName.getText(), fldSurname.getText(), fldEmailSignUp.getText(), choiceEduDeg.getSelectionModel().getSelectedItem(), fldPassSignUp.getText());
+            dao.insertNewUser(user);
+
+            openSignInAction();
+        }
     }
 
     public void signInAction () throws IOException {
         if(fldEmail.getText().isEmpty() || fldPass.getText().isEmpty()) {
             if (fldEmail.getText().isEmpty()) {
+                fldEmail.getStyleClass().add("myborderedregion");
                 fldEmail.getStyleClass().removeAll("validField");
                 fldEmail.getStyleClass().add("invalidField");
             } else {
+                fldEmail.getStyleClass().remove("myborderedregion");
                 fldEmail.getStyleClass().removeAll("invalidField");
                 fldEmail.getStyleClass().add("validField");
             }
 
             if (fldPass.getText().isEmpty()) {
+                fldPass.getStyleClass().add("myborderedregion");
                 fldPass.getStyleClass().removeAll("validField");
                 fldPass.getStyleClass().add("invalidField");
             } else {
+                fldPass.getStyleClass().remove("myborderedregion");
                 fldPass.getStyleClass().removeAll("invalidField");
                 fldPass.getStyleClass().add("validField");
             }
         }
         else {
+            fldPass.getStyleClass().remove("myborderedregion"); //uklanja se crveni obrub ukoliko nisu prazna polja
+            fldEmail.getStyleClass().remove("myborderedregion");
             User user = dao.getUser(fldEmail.getText(), fldPass.getText());
             if(user == null) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
